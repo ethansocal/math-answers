86ad756bf337544126762b1ab20ddf92395c6427
Ethan Henry <79533577+ethansocal@users.noreply.github.com>
2024-08-21 14:42:11 -0700

    Finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { useLocalStorage } from "@uidotdev/usehooks";
 import chapters from "./assets/calc_10e_chapters.xml?raw";
 import TOC from "./assets/table_of_contents.json";
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from "./components/ui/accordion";
+import { Textarea } from "./components/ui/textarea";
+import { Button } from "./components/ui/button";
+import { Select } from "./components/ui/select";
 
 function ProblemInput({
     problems,
@@ -11,21 +20,25 @@ function ProblemInput({
     setProblems: (problems: string) => void;
 }) {
     return (
-        <textarea
-            name="Problems"
-            cols={40}
+        <Textarea
+            placeholder={
+                "Should be in the format:\n755/1,5,11,17,19,25,27,33\n763/45ab,51bc,53,73,75\netc."
+            }
             rows={5}
+            name="Problems"
             value={problems}
             onInput={(e) => {
                 setProblems((e.target as HTMLTextAreaElement).value);
             }}
-        ></textarea>
+        ></Textarea>
     );
 }
 
 interface Problem {
     problem: string;
     page: string;
+    chapter: string;
+    section: string;
 }
 
 function parseProblemInput(input: string): Problem[] {
@@ -38,9 +51,12 @@ function parseProblemInput(input: string): Problem[] {
     for (const line of input.split("\n")) {
         if (line.split("/").length !== 2) continue;
         const [page, problemList] = line.split("/");
+        const { chapter, section } = findProblem(parseInt(page))!;
         if (problemList.split(",").length === 0) continue;
         for (const problem of problemList.split(",")) {
             problems.push({
+                chapter,
+                section,
                 problem,
                 page,
             });
@@ -49,16 +65,29 @@ function parseProblemInput(input: string): Problem[] {
     return problems;
 }
 
-function findProblem(problem: Problem) {
-    const chapter = TOC.find((i) => i.page >= parseInt(problem.page));
-    if (!chapter) return null;
-    return {
-        chapter,
-        problem:
-            chapter.section === "R"
-                ? `Review Exercise ${problem.problem}`
-                : `Problem ${problem.problem}`,
-    };
+function findProblem(page: number) {
+    let last = null;
+    for (const section of TOC) {
+        if (section.page > page) return last;
+        last = section;
+    }
+    return last;
+}
+
+const xml = new DOMParser().parseFromString(chapters, "text/xml");
+const baseUrl = xml.querySelector("CHAPTERS")?.getAttribute("SOLUTIONART");
+const filetype = xml.querySelector("CHAPTERS")?.getAttribute("FILETYPE");
+
+function getProblemUrl(problem: Problem) {
+    const { chapter, section, problem: problemNumber } = problem;
+    const element = xml.querySelector(
+        `CHAPTER[NAME='${chapter}'] > SECTION[NAME='${section}']`
+    );
+    if (!element) return null;
+    return `${baseUrl}/${element.getAttribute("PRE")}${problemNumber.padStart(
+        3,
+        "0"
+    )}.${filetype}`;
 }
 
 function App() {
@@ -67,23 +96,83 @@ function App() {
         () => parseProblemInput(problems),
         [problems]
     );
+    const [expanded, setExpanded] = useState<string[]>([]);
+    const [missing, setMissing] = useState<string[]>([]);
+
     return (
-        <>
+        <div className="p-3">
+            <header className="border-b border-black mb-4">
+                <h1 className="text-2xl">Calculus Solution Viewer</h1>
+            </header>
+            <h1>Copy and paste the assignment numbers below:</h1>
             <ProblemInput problems={problems} setProblems={setProblems} />
+            <h1>Homework history (by due date):</h1>
             {parsedProblems.length > 0 && (
                 <>
                     <h1>Detected Problems:</h1>
-                    <ul>
+                    <div className="flex flex-row gap-3">
+                        <Button onClick={() => setExpanded([])}>
+                            Collapse All
+                        </Button>
+                        <Button
+                            onClick={() =>
+                                setExpanded(
+                                    parsedProblems.map((i) => JSON.stringify(i))
+                                )
+                            }
+                        >
+                            Expand All
+                        </Button>
+                    </div>
+                    <Accordion
+                        type="multiple"
+                        value={expanded}
+                        onValueChange={(e) => {
+                            setExpanded(e);
+                        }}
+                    >
                         {parsedProblems.map((i) => (
-                            <li key={JSON.stringify(i)}>
-                                pg. {i.page} #{i.problem}
-                            </li>
+                            <AccordionItem
+                                value={JSON.stringify(i)}
+                                key={JSON.stringify(i)}
+                            >
+                                <AccordionTrigger>
+                                    pg. {i.page} - {i.chapter}.{i.section} #
+                                    {i.problem}
+                                </AccordionTrigger>
+                                <AccordionContent>
+                                    {!missing.includes(getProblemUrl(i)!) ? (
+                                        <img
+                                            src={getProblemUrl(i) || ""}
+                                            onError={(e) => {
+                                                setMissing((prev) => [
+                                                    ...prev,
+                                                    getProblemUrl(i)!,
+                                                ]);
+                                            }}
+                                        />
+                                    ) : (
+                                        <p className="p-3 bg-red-200 dark:bg-red-800 text-red-800 dark:text-red-300 rounded-lg font-semibold">
+                                            Solution not found! If you have a
+                                            solution for this problem and would
+                                            like it added for others to see,
+                                            please email{" "}
+                                            <a
+                                                href="malito:ethanparkerhenry@gmail.com"
+                                                className="underline text-blue-800 dark:text-blue-300"
+                                            >
+                                                ethanparkerhenry@gmail.com
+                                            </a>
+                                            .
+                                        </p>
+                                    )}
+                                </AccordionContent>
+                            </AccordionItem>
                         ))}
-                    </ul>
+                    </Accordion>
                 </>
             )}
-            <img src="https://ee66b321b2e0a22db2bc-646e12b456b1588b6ef5c188c33a829b.ssl.cf2.rackcdn.com/se11c01005.png" />
-        </>
+        </div>
     );
 }
 
