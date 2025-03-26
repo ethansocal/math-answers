fe07f707740886dfeb7701983ef7de24635433bc
Ethan Henry <ehtanhnery@gmail.com>
2025-03-25 23:01:42 -0700

    Add tailwind 4 and tracking

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,22 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { ThemeProvider } from "./components/theme-provider.tsx";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+    dsn: "https://4a1b6ac5469cd0c5a8bb94bea519da4f@o1112946.ingest.us.sentry.io/4509042480381952",
+    integrations: [Sentry.browserTracingIntegration()],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+    // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+    tracePropagationTargets: [
+        "localhost",
+        /^http(s)?:\/\/ethansocal.github.io\/math-answers.+/,
+    ],
+});
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,27 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        sentryVitePlugin({
+            org: "ethanhenry",
+            project: "math-answers",
+        }),
+    ],
+
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
         },
     },
+
     base: "/math-answers/",
+
+    build: {
+        sourcemap: true,
+    },
 });
