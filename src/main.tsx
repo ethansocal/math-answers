3d03bf9ba70e8b02ba2bf1c6f1ad8f7a2b6bc37d
Ethan Henry <ethanparkerhenry@gmail.com>
2025-03-26 09:32:53 -0700

    add replays

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,10 @@ import * as Sentry from "@sentry/react";
 
 Sentry.init({
     dsn: "https://4a1b6ac5469cd0c5a8bb94bea519da4f@o1112946.ingest.us.sentry.io/4509042480381952",
-    integrations: [Sentry.browserTracingIntegration()],
+    integrations: [
+        Sentry.browserTracingIntegration(),
+        Sentry.replayIntegration(),
+    ],
 
     // Set tracesSampleRate to 1.0 to capture 100%
     // of transactions for performance monitoring.
@@ -18,6 +21,9 @@ Sentry.init({
         "localhost",
         /^http(s)?:\/\/ethansocal.github.io\/math-answers.+/,
     ],
+    // Session Replay
+    replaysSessionSampleRate: 1.0, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
 });
 
 createRoot(document.getElementById("root")!).render(
